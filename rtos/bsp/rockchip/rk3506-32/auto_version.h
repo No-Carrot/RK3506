f257ff93afb19ef5cb1d197b78be760088ab4541
No-Carrot <2286737613@qq.com>
2026-09-01 17:05:18 +0800

    rtos

---
+++ rtos/bsp/rockchip/rk3506-32/auto_version.h
@@ -0,0 +1,7 @@
+#ifndef __AUTO_VERSION_H
+#define __AUTO_VERSION_H
+
+/* This file is auto generate, please do not edit it. */
+#define FIRMWARE_AUTO_VERSION "2025-12-02 09:50:28.908076 e3231e619(rtt) e3231e619(hal)"
+
+#endif
